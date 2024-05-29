671c2c6c7538cb1e3813f3bb9a140ad0cffba53f
DragonToast <153626100+DragonToast@users.noreply.github.com>
2024-05-29 21:36:25 +0200

    Improved the toggle for the cardItem item class. (bound to change when I learn more about DataComponents)

--- src/main/java/com/dragontoast/sorcerycards/Item/custom/CardItem.java
+++ src/main/java/com/dragontoast/sorcerycards/Item/custom/CardItem.java
@@ -1,71 +1,64 @@
 package com.dragontoast.sorcerycards.Item.custom;
 
-import com.mojang.serialization.Codec;
 import net.minecraft.core.Holder;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.common.Tags;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.stream.Stream;
 
 public class CardItem extends Item {
 
 
-    private static final Logger log = LoggerFactory.getLogger(CardItem.class);
     private final int cardValue;
     private final String cardSuit;
     private boolean isActive = false;
-    private boolean isActiveWasChanged = false;
+    private boolean changedIsActive = false;
 
     public CardItem(Properties pProperties, int pCardValue, String pCardSuit) {
         super(pProperties);
         cardValue = pCardValue;
         cardSuit = pCardSuit;
     }
-
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        if (pPlayer.isShiftKeyDown() && !isActiveWasChanged) {
-            if (isActive) {
-                isActive = false;
-                isActiveWasChanged = true;
-            } else {
-                isActive = true;
-                isActiveWasChanged = true;
-            }
-            return InteractionResultHolder.success(pPlayer.getItemInHand(pUsedHand));
-        }
-        else if (pPlayer.isShiftKeyDown() && isActiveWasChanged) {
-            isActiveWasChanged = false;
-            return super.use(pLevel, pPlayer, pUsedHand);
-        }
-        else{
-            return InteractionResultHolder.fail(pPlayer.getItemInHand(pUsedHand));
-        }
+    public void verifyComponentsAfterLoad(ItemStack pStack) {
+        isActive = Boolean.TRUE.equals(pStack.get(DataComponents.ENCHANTMENT_GLINT_OVERRIDE));
+        super.verifyComponentsAfterLoad(pStack);
     }
 
     @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+        if (pPlayer.isShiftKeyDown() && !changedIsActive) {
+            if (isActive) {
+                isActive = false;
+                stack.set(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, false);
+            } else {
+                isActive = true;
+                stack.set(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true);
+            }
+            changedIsActive = true;
+            return InteractionResultHolder.success(stack);
+        }
+        else if (pPlayer.isShiftKeyDown() && changedIsActive) {
+            changedIsActive = false;
+            return super.use(pLevel, pPlayer, pUsedHand);
+        }
+        else{
+            return InteractionResultHolder.fail(stack);
+        }
+    }
+
+
+    @Override
     public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-        if(isActive) {
+        if(pStack.get(DataComponents.ENCHANTMENT_GLINT_OVERRIDE)) {
             if (pEntity instanceof Player) {
                 Holder<MobEffect> effect = switch (cardSuit) {
                     case "hearts" -> MobEffects.HEALTH_BOOST;
@@ -76,7 +69,7 @@ public class CardItem extends Item {
                     default -> null;
                 };
                 if (effect != null) {
-                    ((Player) pEntity).addEffect(new MobEffectInstance(effect, 600, cardValue));
+                    ((Player) pEntity).addEffect(new MobEffectInstance(effect, 20, cardValue));
                 }
             }
         }
